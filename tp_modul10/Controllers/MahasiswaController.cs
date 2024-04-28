5955dbcf03811bface850a2e2bd3a494108b14b2
Orin10120 <rindang3001@gmail.com>
2024-04-28 16:00:12 +0700

    Add project files.

---
+++ tp_modul10/Controllers/MahasiswaController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace tp_modul10.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa> { 
+            new Mahasiswa("Rindang Bani Isyan", "1302223023"),
+            new Mahasiswa("Kevin Albany Junaid", "1302223027"),
+            new Mahasiswa("Muhammad Farhan Ismali Fentarto", "1302220046"),
+            new Mahasiswa("Adrian Fahren Setiawan", "1302220018"),
+            new Mahasiswa("Adib Faizulhaq Armadhani", "1302223110")
+        };
+
+       
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswaList[id];
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Mahasiswa mahasiswa)
+        {
+            mahasiswaList.Add(mahasiswa);
+            return CreatedAtAction(nameof(Get), new{id = mahasiswaList.IndexOf(mahasiswa)} , mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            mahasiswaList.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
---
+++ tp_modul10/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tp_modul10
+{
+    public class Mahasiswa
+    {
+        public String name {  get; set; }
+        public String nim {  get; set; }
+
+        public Mahasiswa(String name, String nim)
+        {
+            this.name = name;
+            this.nim = nim;
+        }
+    }
+}
